261fb888b0f7aee1ef721b00ac2f05a7c3cc64a8
pr3ssh <x@pr3ssh.net>
2020-03-03 14:23:49 +0100

    Working with ENV vars

--- tipi_data/config.py
+++ tipi_data/config.py
@@ -1,10 +1,8 @@
-MONGO_DB = 'tipidb'
-MONGO_HOST = 'localhost'
-MONGO_PORT = 27017
-MONGO_USER = ''
-MONGO_PASSWORD = ''
+from os import environ as env
 
-try:
-    from local_config import *
-except:
-    pass
+
+MONGO_HOST = env.get('MONGO_HOST', 'mongo')
+MONGO_DB = env.get('MONGO_DB_NAME', 'tipidb')
+MONGO_PORT = int(env.get('MONGO_PORT', '27017'))
+MONGO_USER = env.get('MONGO_USER', 'tipi')
+MONGO_PASSWORD = env.get('MONGO_PASSWORD', 'tipi')
