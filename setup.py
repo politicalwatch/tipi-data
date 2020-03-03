d3772e553948d39310fb34243d271fc5f40af9ce
pr3ssh <x@pr3ssh.net>
2020-03-03 14:24:08 +0100

    Add requirements to setup dependencies

--- setup.py
+++ setup.py
@@ -8,6 +8,8 @@ setup(
     author='pr3ssh',
     packages=find_packages(),
     install_requires=[
-        'mongoengine',
+        'marshmallow-mongoengine==0.9.1',
+        'mongoengine==0.16.2',
+        'python-pcre==0.7'
     ],
 )
