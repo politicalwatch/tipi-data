bd7f7a0da8bb54def81a84b3c901f993733c15a0
Víctor Ramírez de la Corte <virako.9@gmail.com>
2020-03-04 08:28:44 +0100

    Add marshmallow dependency

--- setup.py
+++ setup.py
@@ -8,6 +8,7 @@ setup(
     author='pr3ssh',
     packages=find_packages(),
     install_requires=[
+        'marshmallow==2.19.2',
         'marshmallow-mongoengine==0.9.1',
         'mongoengine==0.16.2',
         'python-pcre==0.7'
