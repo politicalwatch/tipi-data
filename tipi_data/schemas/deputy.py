9465e555fd81002042aa922a65d0bc89daa4e4a9
pr3ssh <x@pr3ssh.net>
2020-05-28 12:02:41 +0200

    Add Deputy EXTRA field

--- tipi_data/schemas/deputy.py
+++ tipi_data/schemas/deputy.py
@@ -14,6 +14,7 @@ class DeputySchema(ma.ModelSchema):
                 'start_date': {'load_only': True},
                 'end_date': {'load_only': True},
                 'url': {'load_only': True},
+                'extra': {'load_only': True},
                 }
 
 
