56dae6726f795ce87f864d0aa793afaab50332df
David Hernandez <davidhernandezruiz@gmail.com>
2021-04-07 15:43:40 +0200

    Fixes field names for video model.

--- tipi_data/models/video.py
+++ tipi_data/models/video.py
@@ -5,9 +5,9 @@ class Video(db.Document):
     id = db.StringField(db_field='_id', primary_key=True)
     reference = db.StringField()
     link = db.StringField()
-    session_type = db.StringField()
+    session_name = db.StringField()
     speaker = db.StringField()
-    link = db.StringField()
+    type = db.StringField()
     date = db.IntField()
 
     meta = {'collection': 'videos'}
