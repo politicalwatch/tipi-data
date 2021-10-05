de503d99f957b1575027dfdb086afaf40d61c6f8
David Hernandez <davidhernandezruiz@gmail.com>
2021-10-05 09:54:18 +0200

    Adds new InitiativeAlerts repository.

--- tipi_data/repositories/alerts.py
+++ tipi_data/repositories/alerts.py
@@ -1,7 +1,29 @@
-from tipi_data.models.alert import InitiativeAlert
+from tipi_data.models.alert import Alert, InitiativeAlert
 
 
 class Alerts():
     @staticmethod
     def get_all():
         return InitiativeAlert.objects()
+
+    @staticmethod
+    def get_validated():
+        return Alert.objects.filter(searches__validated=True)
+
+class InitiativeAlerts():
+    @staticmethod
+    def get_all():
+        return InitiativeAlert.objects()
+
+    @staticmethod
+    def clear():
+        InitiativeAlert.drop_collection()
+
+    @staticmethod
+    def by_search(search, kb):
+        query = search
+        query['tagged.knowledgebase'] = kb
+        return InitiativeAlert.objects(
+            __raw__=query
+        )
+
