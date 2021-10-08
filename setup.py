6a760f2008ef7ff4f12eb2f7e4ca4dd84da971e0
pr3ssh <x@pr3ssh.net>
2021-10-08 22:49:39 +0200

    Adds function to generate slugs from args

--- setup.py
+++ setup.py
@@ -12,6 +12,7 @@ setup(
         'marshmallow-mongoengine==0.9.1',
         'mongoengine==0.16.2',
         'python-pcre==0.7',
-        'natsort==7.0.1'
+        'natsort==7.0.1',
+        'python-slugify==5.0.2'
     ],
 )
--- tipi_data/utils.py
+++ tipi_data/utils.py
@@ -1,4 +1,6 @@
 import hashlib
+from slugify import slugify
+from uuid import uuid4
 
 
 def generate_id(*args):
@@ -8,3 +10,8 @@ def generate_id(*args):
                 ).hexdigest()
     except Exception:
         return 'ID_ERROR'
+
+def generate_slug(*args):
+    return "{}-{}".format(
+            slugify( u''.join(args).encode('utf-8')),
+            str(uuid4()).split('-')[0])
