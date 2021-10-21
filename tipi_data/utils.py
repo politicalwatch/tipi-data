536aa2c0821c78673dc2efa9075f2f75d98563bd
pr3ssh <x@pr3ssh.net>
2021-10-21 12:20:38 +0200

    Removes random string from slugs

--- tipi_data/utils.py
+++ tipi_data/utils.py
@@ -1,6 +1,5 @@
 import hashlib
 from slugify import slugify
-from uuid import uuid4
 
 
 def generate_id(*args):
@@ -12,6 +11,4 @@ def generate_id(*args):
         return 'ID_ERROR'
 
 def generate_slug(*args):
-    return "{}-{}".format(
-            slugify( u''.join(args).encode('utf-8')),
-            str(uuid4()).split('-')[0])
+    return slugify( u''.join(args).encode('utf-8'))
