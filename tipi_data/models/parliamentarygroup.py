f15eea1f4a6e006231a98b62c3d93d087a7f2356
pr3ssh <x@pr3ssh.net>
2022-01-13 11:53:19 +0100

    Removes active field on parliamentarygroup

--- tipi_data/models/parliamentarygroup.py
+++ tipi_data/models/parliamentarygroup.py
@@ -5,7 +5,6 @@ class ParliamentaryGroup(db.Document):
     id = db.StringField(db_field='_id', primary_key=True)
     name = db.StringField()
     shortname = db.StringField()
-    active = db.BooleanField()
 
     meta = {
             'collection': 'parliamentarygroups',
--- tipi_data/schemas/parliamentarygroup.py
+++ tipi_data/schemas/parliamentarygroup.py
@@ -6,7 +6,3 @@ from tipi_data.models.parliamentarygroup import ParliamentaryGroup
 class ParliamentaryGroupSchema(ma.ModelSchema):
     class Meta:
         model = ParliamentaryGroup
-        model_skip_values = [None]
-        model_fields_kwargs = {
-                'active': {'load_only': True}
-                }
