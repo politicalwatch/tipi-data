9d52c3a70803203883966af425ae7d662e8369c6
David Hernández <davidhernandezruiz@gmail.com>
2022-04-06 15:37:29 +0200

    Adapts scanned model to new tagged system.

--- tipi_data/models/scanned.py
+++ tipi_data/models/scanned.py
@@ -1,19 +1,26 @@
 from tipi_data import db
-from tipi_data.models.initiative import Tag
+from tipi_data.models.initiative import Tagged
 
-
-class ScannedResult(db.DynamicEmbeddedDocument):
-    topics = db.ListField(db.StringField(), default=list)
-    tags = db.EmbeddedDocumentListField(Tag, default=list)
 
 class Scanned(db.Document):
     id = db.StringField(db_field='_id', primary_key=True)
     title = db.StringField()
     excerpt = db.StringField()
-    result = db.EmbeddedDocumentField(ScannedResult)
+    result = db.EmbeddedDocumentListField(Tagged, default=list)
     created = db.DateTimeField()
     expiration = db.DateTimeField()
     verified = db.BooleanField()
 
     meta = {'collection': 'scanned'}
     # TODO Add indexes https://docs.mongoengine.org/guide/defining-documents.html#indexes
+
+    def add_tag(self, kb, topic, subtopic, tag_name, times):
+        tagged = list(filter(lambda tagged: tagged.knowledgebase == kb, self.result))
+
+        if len(tagged) > 0:
+            tagged = tagged[0]
+        else:
+            tagged = Tagged(knowledgebase=kb, topics=[], tags=[])
+            self.result.append(tagged)
+
+        tagged.add_tag(topic, subtopic, tag_name, times)
