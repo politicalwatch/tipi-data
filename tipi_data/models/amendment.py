090dd86d14ebc55a0d9f4803b64f256ae46d35b9
David Hernández <davidhernandezruiz@gmail.com>
2022-12-15 18:38:14 +0100

    Adds new functionality to the amendment model and new functions to the amendments repository.

--- tipi_data/models/amendment.py
+++ tipi_data/models/amendment.py
@@ -2,6 +2,63 @@ import hashlib
 
 from tipi_data import db
 
+class Tag(db.EmbeddedDocument):
+    topic = db.StringField()
+    subtopic = db.StringField()
+    tag = db.StringField()
+    times = db.IntField()
+
+    def __str__(self):
+        return self.tag
+
+    def serialize(self):
+        return {
+            'topic': self.topic,
+            'subtopic': self.subtopic,
+            'tag': self.tag,
+            'times': self.times
+        }
+
+class Tagged(db.EmbeddedDocument):
+    knowledgebase = db.StringField()
+    topics = db.ListField(db.StringField(), default=list)
+    tags = db.EmbeddedDocumentListField(Tag, default=list)
+
+    def __str__(self):
+        return self.knowledgebase
+
+    def add_topic(self, topic):
+        if topic not in self.topics:
+            self.topics.append(topic)
+
+    def add_tag(self, topic, subtopic, tag_name, times):
+        if list(filter(lambda tag: tag.tag == tag_name and tag.subtopic == subtopic, self.tags)) == []:
+            tag = Tag(topic=topic, subtopic=subtopic, tag=tag_name, times=times)
+            self.tags.append(tag)
+            self.add_topic(topic)
+
+    def remove_single_occurences(self):
+        topics_counter = dict()
+        for tag in self.tags:
+            if tag['topic'] in topics_counter.keys():
+                topics_counter[tag['topic']] += tag['times']
+            else:
+                topics_counter[tag['topic']] = tag['times']
+        for key in topics_counter.keys():
+            if topics_counter[key] == 1:
+                self.tags = list(filter(lambda x: x['topic'] != key, self.tags))
+        self.topics = sorted(list(set([tag['topic'] for tag in self.tags])))
+
+    def has_topics(self):
+        return len(self.topics) > 0
+
+    def serialize(self):
+        return {
+            'knowledgebase': self.knowledgebase,
+            'topics': self.topics,
+            'tags': list(map(lambda tag_set: tag_set.serialize(), self.tags))
+        }
+
 class Amendment(db.Document):
     id = db.StringField(db_field='_id', primary_key=True)
     bulletin_name = db.StringField()
@@ -13,6 +70,8 @@ class Amendment(db.Document):
     justification = db.ListField(db.StringField(), default=list)
     propossed_change = db.ListField(db.StringField(), default=list)
     from_senate = db.BooleanField()
+    justification_tagged = db.EmbeddedDocumentListField(Tagged, default=list)
+    propossed_change_tagged = db.EmbeddedDocumentListField(Tagged, default=list)
 
     def set_id(self, id):
         self.id = self.reference + '/' + hashlib.md5(self.bulletin_name.encode()).hexdigest() + '/' + id.strip()
@@ -21,7 +80,7 @@ class Amendment(db.Document):
         self.author_deputies.append(author)
 
     def add_type(self, type):
-        self.type = type
+        self.type = type.capitalize()
 
     def add_applies_to(self, applies_to):
         self.applies_to = applies_to
@@ -40,3 +99,25 @@ class Amendment(db.Document):
 
     def mark_as_senate(self):
         self.from_senate = True
+
+    def add_justification_tag(self, kb, topic, subtopic, tag_name, times):
+        tagged = list(filter(lambda tagged: tagged.knowledgebase == kb, self.justification_tagged))
+
+        if len(tagged) > 0:
+            tagged = tagged[0]
+        else:
+            tagged = Tagged(knowledgebase=kb, topics=[], tags=[])
+            self.justification_tagged.append(tagged)
+
+        tagged.add_tag(topic, subtopic, tag_name, times)
+
+    def add_propossed_change_tag(self, kb, topic, subtopic, tag_name, times):
+        tagged = list(filter(lambda tagged: tagged.knowledgebase == kb, self.propossed_change_tagged))
+
+        if len(tagged) > 0:
+            tagged = tagged[0]
+        else:
+            tagged = Tagged(knowledgebase=kb, topics=[], tags=[])
+            self.propossed_change_tagged.append(tagged)
+
+        tagged.add_tag(topic, subtopic, tag_name, times)
