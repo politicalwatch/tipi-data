31fa5b66d1299f87ed050fe5cf1385d43f110055
David Hernández <davidhernandezruiz@gmail.com>
2023-01-03 12:24:37 +0100

    Adds function to get all untagged amendments to the amendments repository.

--- tipi_data/repositories/amendments.py
+++ tipi_data/repositories/amendments.py
@@ -9,3 +9,22 @@ class Amendments:
    @staticmethod 
    def by_reference_and_bulletin(reference, bulletin):
         return Amendment.objects.filter(reference=reference, bulletin_name=bulletin)
+
+   @staticmethod
+   def get_all_untagged():
+       query = {
+               '$or': [
+                   {'justification_tagged': []},
+                   {'justification_tagged': {'$exists': False}},
+                   {'propossed_change_tagged': []},
+                   {'propossed_change_tagged': {'$exists': False}},
+                   ]
+               }
+       return Amendments.by_query(query)
+
+   @staticmethod
+   def by_query(query):
+       if '$text' in query.keys():
+           return Amendment.objects(__raw__=query).order_by()
+
+       return Amendment.objects(__raw__=query)
