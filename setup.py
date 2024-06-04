69621f664e0c93dd1fc76f63f672ac7903df5ada
Alex Ahumada <projectsperminute@gmail.com>
2024-06-04 16:00:58 +0200

    Update mongoengine dependencies

--- setup.py
+++ setup.py
@@ -1,18 +1,18 @@
 from setuptools import setup, find_packages
 
 setup(
-    name='tipi-data',
-    version='1.0.0',
-    description='TIPI Data',
-    url='https://github.com/politicalwatch/tipi-data',
-    author='pr3ssh',
+    name="tipi-data",
+    version="1.0.0",
+    description="TIPI Data",
+    url="https://github.com/politicalwatch/tipi-data",
+    author="pr3ssh",
     packages=find_packages(),
     install_requires=[
-        'marshmallow==2.19.2',
-        'marshmallow-mongoengine==0.9.1',
-        'mongoengine==0.16.2',
-        'python-pcre==0.7',
-        'natsort==7.0.1',
-        'python-slugify==5.0.2'
+        "marshmallow==3.21.2",
+        "marshmallow-mongoengine==0.31.2",
+        "mongoengine==0.28.2",
+        "python-pcre==0.7",
+        "natsort==7.0.1",
+        "python-slugify==5.0.2",
     ],
 )
