612e51ca762e3c5fdf8d2b69c08c5b6dd9e54bb4
Alex Ahumada <projectsperminute@gmail.com>
2024-06-06 08:54:35 +0200

    Update dependencies

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="tipi-data",
-    version="1.0.0",
+    version="1.1.0",
     description="TIPI Data",
     url="https://github.com/politicalwatch/tipi-data",
     author="pr3ssh",
@@ -12,7 +12,7 @@ setup(
         "marshmallow-mongoengine==0.31.2",
         "mongoengine==0.28.2",
         "python-pcre==0.7",
-        "natsort==7.0.1",
-        "python-slugify==5.0.2",
+        "natsort==7.1.1",
+        "python-slugify==8.0.4",
     ],
 )
