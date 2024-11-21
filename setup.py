1b0b9fbf469c0cb736de4f6b16c6cb74d0f8553a
Alex Ahumada <projectsperminute@gmail.com>
2024-11-21 12:17:48 +0100

    Update dependencies

--- setup.py
+++ setup.py
@@ -8,9 +8,9 @@ setup(
     author="pr3ssh",
     packages=find_packages(),
     install_requires=[
-        "marshmallow==3.21.2",
+        "marshmallow==3.22.0",
         "marshmallow-mongoengine==0.31.2",
-        "mongoengine==0.28.2",
+        "mongoengine==0.29.1",
         "regex==2024.11.6",
         "natsort==7.1.1",
         "python-slugify==8.0.4",
